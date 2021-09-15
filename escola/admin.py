2d9a61cff4ff2ad17d680d5c2065f5a23abdff2c
Gustavobr20 <gustavo.henrique.oliveira50@gmail.com>
2021-09-15 12:54:13 -0300

    api finalizada

---
+++ escola/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from escola.models import Aluno
+
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
